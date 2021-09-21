ea99234fb39e1758e0abab33dc2ac7686ed7ba2f
Mohsin <mohsin.awan0@gmail.com>
2021-09-21 16:47:19 +0500

    Token show

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded())
 app.use('/hubspot', hubspotRouter);
 app.get('/', async(req, res) => {
-    res.send('I am working');
+    const accessToken = fs.readFile('token.txt','utf8', async function(err, data) {
+        return data;
+    });
+
+    res.send(accessToken);
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
